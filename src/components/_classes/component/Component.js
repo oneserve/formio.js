a386dbd52319cec4d0d3a6a06a195bf17b2e67f6
alexandraRamanenka <sasha@form.io>
2021-04-15 12:33:45 +0300

    Added Display Mask option

--- src/components/_classes/component/Component.js
+++ src/components/_classes/component/Component.js
@@ -73,7 +73,7 @@ export default class Component extends Element {
       multiple: false,
 
       /**
-       * The default value of this compoennt.
+       * The default value of this component.
        */
       defaultValue: null,
 
@@ -706,7 +706,8 @@ export default class Component extends Element {
         (!recursion && (key === 'input')) ||
         (!recursion && (key === 'tableView')) ||
         (val !== '' && !defaultSchema.hasOwnProperty(key)) ||
-        (val !== '' && val !== defaultSchema[key])
+        (val !== '' && val !== defaultSchema[key]) ||
+        (defaultSchema[key] && val !== defaultSchema[key])
       ) {
         modified[key] = val;
       }
--- src/components/_classes/input/Input.js
+++ src/components/_classes/input/Input.js
@@ -74,7 +74,7 @@ export default class Input extends Multivalue {
   }
 
   setInputMask(input, inputMask) {
-    return super.setInputMask(input, (inputMask || this.component.inputMask), !this.component.placeholder);
+    return super.setInputMask(input, (inputMask || this.component.displayMask || this.component.inputMask), !this.component.placeholder);
   }
 
   getMaskOptions() {
